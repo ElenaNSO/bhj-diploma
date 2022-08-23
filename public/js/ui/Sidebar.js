c722c5acfb19dd78237c5d2fdf0dcd570d6b5a6d
ElenaNSO <yaelena@ngs.ru>
2022-08-23 17:45:52 +0700

    feat: add ui/Sidebar.js

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -18,7 +18,22 @@ class Sidebar {
    * при нажатии на кнопку .sidebar-toggle
    * */
   static initToggleButton() {
+    const sidebarPanelVisible = document.querySelector('.sidebar-mini');
+    const sidebarToggle = document.querySelectorAll('.visible-xs');
 
+    document.addEventListener('click', event => {
+      
+      if(event.target.className.includes('visible-xs')) {
+        if(sidebarPanelVisible.className.includes('sidebar-open')) {
+            sidebarPanelVisible.classList.remove('sidebar-open')
+            sidebarPanelVisible.classList.remove('sidebar-collapse')
+        }else {
+            sidebarPanelVisible.classList.add('sidebar-open');
+            sidebarPanelVisible.classList.remove('sidebar-collapse')
+
+        }
+      }
+    })
   }
 
   /**
@@ -29,6 +44,29 @@ class Sidebar {
    * выходу устанавливает App.setState( 'init' )
    * */
   static initAuthLinks() {
-
+      const menuSideBar = document.querySelectorAll('.sidebar-menu');
+      menuSideBar.forEach(elment => {
+        elment.addEventListener('click', event => {
+          if(event.target.parentElement.parentElement.className.includes('menu-item_login')) {
+              event.preventDefault();
+              App.getModal('login').open();
+          };
+          if(event.target.parentElement.parentElement.className.includes('menu-item_register')) {
+                event.preventDefault();
+                App.getModal('register').open();
+            }; 
+          if(event.target.parentElement.parentElement.className.includes('menu-item_logout')) {
+                      event.preventDefault();
+                      User.logout((err,resp) => {
+                          if(resp && resp.success) {
+                              App.setState('init');
+                          }
+                      });
+          };
+              
+            
+        })
+      })
+    }
   }
-}
+  
