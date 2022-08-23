a9dd67bec582d4081d048f61c62104b61f35274f
ElenaNSO <yaelena@ngs.ru>
2022-08-23 17:44:43 +0700

    feat: add ui/Modal.js

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -12,7 +12,13 @@ class Modal {
    * необходимо выкинуть ошибку.
    * */
   constructor(element){
+    this.element = element;
+    this.registerEvents();
 
+   if(element === undefined) {
+      const errorGetElement = new Error('Елемент не существует');
+      throw errorGetElement;
+   }
   }
 
   /**
@@ -21,27 +27,35 @@ class Modal {
    * (с помощью метода Modal.onClose)
    * */
   registerEvents() {
-
-  }
+   this.element.querySelectorAll('[data-dismiss="modal"]').forEach(element => {
+      element.onclick = event => {
+      this.onClose(event);
+      }
+    });
+    
+    }
+  
 
   /**
    * Срабатывает после нажатия на элементы, закрывающие окно.
    * Закрывает текущее окно (Modal.close())
    * */
   onClose(e) {
-
+    e.preventDefault();
+    this.close();
   }
   /**
    * Открывает окно: устанавливает CSS-свойство display
    * со значением «block»
    * */
   open() {
-
+    this.element.style.display = 'block';           
   }
   /**
    * Закрывает окно: удаляет CSS-свойство display
    * */
   close(){
-
+    this.element.style.display = 'none';  
+    
   }
 }
