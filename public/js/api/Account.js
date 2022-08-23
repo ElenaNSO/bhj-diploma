a7585934c7da16121214cd4c21fb5350949a275e
ElenaNSO <yaelena@ngs.ru>
2022-08-23 17:38:23 +0700

    feat: add api/Account.js

--- public/js/api/Account.js
+++ public/js/api/Account.js
@@ -4,10 +4,12 @@
  * Имеет свойство URL со значением '/account'
  * */
 class Account extends Entity {
-  /**
-   * Получает информацию о счёте
-   * */
-  static get(id = '', callback){
-
+  static get(id = '', callback) {
+    createRequest ({
+      url: this.URL + '/' + id,
+      method: 'GET',
+      callback
+    })
   }
 }
+Account.URL = '/account';
