10e41dbf5c9232ed6285dc9356fce76de774e943
ElenaNSO <yaelena@ngs.ru>
2022-08-23 17:51:00 +0700

    feat: add  ui/forms/RegisterForm.js

--- public/js/ui/forms/RegisterForm.js
+++ public/js/ui/forms/RegisterForm.js
@@ -11,5 +11,17 @@ class RegisterForm extends AsyncForm {
    * */
   onSubmit(data) {
 
+    User.register(data, (err,resp) => {
+      if(resp && resp.user) {
+        this.element.reset();
+        App.setState('user-logged');
+        App.getModal('register').close();
+        UserWidget.update();     //+
+      }else {
+        this.element.reset();
+        App.getModal('register').close();
+      }
+    })
   }
 }
+
